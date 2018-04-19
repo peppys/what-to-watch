d18e0eb61e2ab05855ca4d5ffab03ee1cbad5342
Peppy Sisay <peppy@tradesy.com>
2018-04-19 15:50:07 -0700

    Take ip/port as flag argument

--- api/main.go
+++ api/main.go
@@ -5,12 +5,17 @@ import (
 	"log"
 	"os"
 	"fmt"
+	"flag"
 )
 
 func main() {
+	ip := flag.String("ip", "", "IP address to use")
+	port := flag.String("port", "8081", "Port to use")
+	flag.Parse()
+
 	http.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
 		w.Write([]byte(fmt.Sprintf("API! Version: %s Env: %s", os.Getenv("VERSION"), os.Getenv("APP_ENV"))))
 	})
 
-	log.Fatal(http.ListenAndServe(":8081", nil))
+	log.Fatal(http.ListenAndServe(fmt.Sprintf("%s:%s", *ip, *port), nil))
 }
--- web/main.go
+++ web/main.go
@@ -5,14 +5,20 @@ import (
 	"log"
 	"os"
 	"fmt"
+	"flag"
 )
 
 func main() {
+	ip := flag.String("ip", "", "IP address to use")
+	port := flag.String("port", "8080", "Port to use")
+	apiURL := flag.String("api", "http://dev-api.peppy.ml", "API address")
+	flag.Parse()
+
 	http.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
 		w.Header().Set("Content-Type", "text/html; charset=utf-8")
 		fmt.Fprintf(w, "WEB! Version: %s Env: %s", os.Getenv("VERSION"), os.Getenv("APP_ENV"))
-		fmt.Fprintf(w, "<a href='%s'>API Link</a>", os.Getenv("API_URL"))
+		fmt.Fprintf(w, "<a href='%s'>API Link</a>", *apiURL)
 	})
 
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	log.Fatal(http.ListenAndServe(fmt.Sprintf("%s:%s", *ip, *port), nil))
 }
