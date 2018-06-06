d27bf9fddeb5ad3fe5f52d28f8202d2a7130fc55
Peppy Sisay <peppy@tradesy.com>
2018-06-05 23:14:58 -0700

    Add CORS for now

--- api/server/http_gateway.go
+++ api/server/http_gateway.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"net/http"
 	"log"
+	"strings"
 
 	pb "github.com/PeppyS/what-to-watch/proto"
 	"github.com/grpc-ecosystem/grpc-gateway/runtime"
@@ -28,6 +29,33 @@ func ListenAndServeHTTPGateway(grpcAddress, httpAddress string) error {
 		return err
 	}
 
+	s := &http.Server{
+		Addr:    httpAddress,
+		Handler: allowCORS(mux),
+	}
+
 	log.Println("Listening on", httpAddress)
-	return http.ListenAndServe(httpAddress, mux)
+	return s.ListenAndServe()
+}
+
+// allowCORS allows Cross Origin Resoruce Sharing from any origin.
+// Don't do this without consideration in production systems.
+func allowCORS(h http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if origin := r.Header.Get("Origin"); origin != "" {
+			w.Header().Set("Access-Control-Allow-Origin", origin)
+			if r.Method == "OPTIONS" && r.Header.Get("Access-Control-Request-Method") != "" {
+				preflightHandler(w, r)
+				return
+			}
+		}
+		h.ServeHTTP(w, r)
+	})
+}
+
+func preflightHandler(w http.ResponseWriter, r *http.Request) {
+	headers := []string{"Content-Type", "Accept"}
+	w.Header().Set("Access-Control-Allow-Headers", strings.Join(headers, ","))
+	methods := []string{"GET", "HEAD", "POST", "PUT", "DELETE"}
+	w.Header().Set("Access-Control-Allow-Methods", strings.Join(methods, ","))
 }
